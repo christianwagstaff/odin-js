bf8fe87b2cce3f132a0671121f76fd2b8f766690
Christian <chris23681@gmail.com>
2021-11-05 14:36:28 -0700

    Adding high score and reset button

--- rock_paper_scissors/rps_v2.js
+++ rock_paper_scissors/rps_v2.js
@@ -1,14 +1,22 @@
 const rpsChoices = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let computerScore = 0;
+let playerHighScoreVal = 0;
+let computerHighScoreVal = 0;
 
 const rockChoice = document.getElementById('rockBtn');
 const paperChoice = document.getElementById('paperBtn');
 const scissorsChoice = document.getElementById('scissorsBtn');
+const playerHighScore = document.getElementById('playerHighScore');
+const computerHighScore = document.getElementById('computerHighScore');
+const playerScoreHTML = document.getElementById('playerScore');
+const computerScoreHTML = document.getElementById('computerScore');
+const resetButton = document.getElementById('reset');
 
 rockChoice.addEventListener('click', () => game('Rock'));
 paperChoice.addEventListener('click', () => game('Paper'));
 scissorsChoice.addEventListener('click', () => game('Scissors'));
+resetButton.addEventListener('click', () => resetGame());
 
 function computerPlay() {
     choice = Math.floor(Math.random() * rpsChoices.length);
@@ -29,8 +37,9 @@ function playRound(playerSelection, computerSelection) {
 
 function game(playerChoice) {
     if (playerScore === 5 || computerScore === 5) {
-        playerScore = 0;
-        computerScore = 0;
+        updateTotalGamesWon();
+        updateHighScoreDispalay();
+        resetCurrentScore();
     }
     let winner = playRound(playerChoice, computerPlay())
     if (winner === 'playerWin') {
@@ -38,10 +47,40 @@ function game(playerChoice) {
     } else if (winner === 'computerWin') {
         computerScore += 1;
     } 
-    updateCurrentScore(playerScore, computerScore);
+    updateCurrentScoreDisplay();
 }
 
-function updateCurrentScore(playerScore, computerScore) {
-    document.getElementById('playerScore').innerHTML = playerScore;
-    document.getElementById('computerScore').innerHTML = computerScore;
+function updateCurrentScoreDisplay() {
+    playerScoreHTML.innerHTML = playerScore;
+    computerScoreHTML.innerHTML = computerScore;
+}
+
+function updateHighScoreDispalay() {
+    playerHighScore.innerHTML = playerHighScoreVal;
+    computerHighScore.innerHTML = computerHighScoreVal;
+}
+
+function updateTotalGamesWon() {
+    if (playerScore === 5) {
+        playerHighScoreVal += 1;
+    } else {
+        computerHighScoreVal += 1;
+    }
+}
+
+function resetCurrentScore() {
+    playerScore = 0;
+    computerScore = 0;
+}
+
+function resetHighScore() {
+    playerHighScoreVal = 0;
+    computerHighScoreVal = 0;
+}
+
+function resetGame() {
+    resetCurrentScore();
+    resetHighScore();
+    updateCurrentScoreDisplay();
+    updateHighScoreDispalay();
 }
