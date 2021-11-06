44fed94d2d84514a61ec698df31526ce5bdbf645
Christian <chris23681@gmail.com>
2021-11-05 17:11:39 -0700

    Display current rounds choices, and hide when reset

--- rock_paper_scissors/rps_v2.js
+++ rock_paper_scissors/rps_v2.js
@@ -3,6 +3,9 @@ let playerScore = 0;
 let computerScore = 0;
 let playerHighScoreVal = 0;
 let computerHighScoreVal = 0;
+let rock = 'far fa-hand-rock'
+let paper = 'far fa-sticky-note';
+let scissors = 'far fa-hand-scissors';
 
 const rockChoice = document.getElementById('rockBtn');
 const paperChoice = document.getElementById('paperBtn');
@@ -12,6 +15,11 @@ const computerHighScore = document.getElementById('computerHighScore');
 const playerScoreHTML = document.getElementById('playerScore');
 const computerScoreHTML = document.getElementById('computerScore');
 const resetButton = document.getElementById('reset');
+const playerCurrentPickDiv = document.getElementById('playerCurrentPickDiv');
+const computerCurrentPickDiv = document.getElementById('computerCurrentPickDiv');
+const playerCurrentPick = document.getElementById('playerCurrentPick');
+const computerCurrentPick = document.getElementById('computerCurrentPick');
+
 
 rockChoice.addEventListener('click', () => game('Rock'));
 paperChoice.addEventListener('click', () => game('Paper'));
@@ -41,13 +49,17 @@ function game(playerChoice) {
         updateHighScoreDispalay();
         resetCurrentScore();
     }
-    let winner = playRound(playerChoice, computerPlay())
+    let computerChoice = computerPlay();
+    let winner = playRound(playerChoice, computerChoice)
     if (winner === 'playerWin') {
         playerScore += 1;
     } else if (winner === 'computerWin') {
         computerScore += 1;
-    } 
+    }
+    updateCurrentPick(playerCurrentPick, playerChoice);
+    updateCurrentPick(computerCurrentPick, computerChoice); 
     updateCurrentScoreDisplay();
+    showCurrentPick();
 }
 
 function updateCurrentScoreDisplay() {
@@ -83,4 +95,25 @@ function resetGame() {
     resetHighScore();
     updateCurrentScoreDisplay();
     updateHighScoreDispalay();
+    hideCurrentPick();
+}
+
+function showCurrentPick() {
+    playerCurrentPickDiv.className = '';
+    computerCurrentPickDiv.className = '';
+}
+
+function hideCurrentPick() {
+    playerCurrentPickDiv.className = 'hidden';
+    computerCurrentPickDiv.className = 'hidden';
+}
+
+function updateCurrentPick(player, choice) {
+    if (choice === 'Rock') {
+        player.className = rock;
+    } else if (choice === "Paper") {
+        player.className = paper;
+    } else {
+        player.className = scissors;
+    }
 }
